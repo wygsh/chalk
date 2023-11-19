e12f949bd7fd6e1008f4a02c6990faf29b8d7851
wygsh <wygsh@outlook.com>
2023-11-19 14:27:18 +0100

    Fix rgb() and run v fmt

--- chalk.v
+++ chalk.v
@@ -9,36 +9,20 @@ const (
 
 pub fn style(str string, style graphics.Styles) string {
 	style_str := graphics.style(style)
-	return (
-		style_str +
-		util.replace_last(str, delim, delim + style_str) +
-		delim
-	)
+	return style_str + util.replace_last(str, chalk.delim, chalk.delim + style_str) + chalk.delim
 }
 
 pub fn color8(str string, color graphics.Colors8, layer graphics.Layer) string {
 	color_str := graphics.color8(color, layer)
-	return (
-		color_str +
-		util.replace_last(str, delim, delim + color_str) +
-		delim
-	)
+	return color_str + util.replace_last(str, chalk.delim, chalk.delim + color_str) + chalk.delim
 }
 
 pub fn color256(str string, color_id graphics.Color256ID, layer graphics.Layer) string {
 	color_str := graphics.color256(color_id, layer)
-	return (
-		color_str +
-		util.replace_last(str, delim, delim + color_str) +
-		delim
-	)
+	return color_str + util.replace_last(str, chalk.delim, chalk.delim + color_str) + chalk.delim
 }
 
-pub fn rgb(str string, rgb graphics.RGB, layer graphics.Layer) !string {
-	rgb_str := graphics.rgb(rgb, layer)!
-	return (
-		rgb_str +
-		util.replace_last(str, delim, delim + rgb_str) +
-		delim
-	)
+pub fn rgb(str string, r u8, g u8, b u8, layer graphics.Layer) string {
+	rgb_str := graphics.rgb(r, g, b, layer)
+	return rgb_str + util.replace_last(str, chalk.delim, chalk.delim + rgb_str) + chalk.delim
 }
--- util/util.v
+++ util/util.v
@@ -1,9 +1,7 @@
 module util
 
 pub fn replace_last(str string, rep string, with string) string {
-	str.index(rep) or {
-		return str
-	}
+	str.index(rep) or { return str }
 
 	return str.all_before_last(rep) + with + str.all_after_last(rep)
 }
